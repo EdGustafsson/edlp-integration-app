548e826d4831bc00ce6d954f820ab719c687df8f
Martin Vehmas <martin.vehmas@viaecole.se>
2021-12-07 10:47:47 +0100

    Added Resource class

---
+++ learnpoint-test-consoleApp/Models/Resource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace learnpoint_test_consoleApp.Models
+{
+    class Resource
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public ExternalId SourceId { get; set; }
+        public ExternalId TargetId { get; set; }
+
+        public class ExternalId
+        {
+            public int IntId { get; set; }
+            public Guid GuidId { get; set; }
+            public IdType Type { get; set; }
+            public string ExternalSystem { get; set; }
+
+            public enum IdType
+            {
+                Int,
+                Guid
+            }
+        }
+    }
+}
